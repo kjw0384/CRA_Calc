2761933092914798bfddbbb0b0f75a0de836a053
JiwnwooHong601 <jinwoo.hong601@gmail.com>
2024-05-29 16:35:58 +0900

    Update cal_test.cpp
    Add zegop tc to UT

--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -1,5 +1,14 @@
 #include "pch.h"
 
+#include "../Project7/cal.cpp"
+
+TEST(CalcTest, ZegopTest) {
+	int zegopResult = getZegop(3);
+
+	EXPECT_EQ(zegopResult, 9);
+	EXPECT_TRUE(true);
+}
+
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
