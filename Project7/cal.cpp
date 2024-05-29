c123f3decb5fce4f0ef3df55374547c9c7fa870f
JuyoungKimmy-Kim <kimmykim.jy@gmail.com>
2024-05-29 16:28:58 +0900

    add getSum

--- Project7/cal.cpp
+++ Project7/cal.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int getSum(int a, int b) { return 0; }
+int getSum(int a, int b) { return a + b ; }
 int getGop(int a, int b) { return 0; }
 int getZegop(int a, int b) { return 0; }
 int getMinus(int a, int b) { return 0; }
