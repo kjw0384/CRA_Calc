11e40b51c5cc84b8bf4ac972f344bcb1dc146a45
sunha <sunha0710@naver.com>
2024-05-29 16:36:14 +0900

    getMinus func

--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -1,6 +1,34 @@
 #include "pch.h"
+#include "../Project7/cal.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestMinus, resultPositive) {
+	
+	int ret = getMinus(10, 5);
+	int exp = 5;
+
+	EXPECT_EQ(ret, exp);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestMinus, resultNegative) {
+
+	int ret = getMinus(3, 5);
+	int exp = -2;
+
+	EXPECT_EQ(ret, exp);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestMinus, resultzero) {
+
+	int ret = getMinus(7, 7);
+	int exp = 0;
+
+	EXPECT_EQ(ret, exp);
+	EXPECT_TRUE(true);
 }
