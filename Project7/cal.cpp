d026fd809d5e39003705f44ddab1f2be68417b84
joonjae7.lee <joonjae7.lee@samsung.com>
2024-05-29 17:12:32 +0900

    add exception for divide

--- Project7/cal.cpp
+++ Project7/cal.cpp
@@ -5,6 +5,10 @@ int getGop(int a, int b) { return 0; }
 int getZegop(int a, int b) { return 0; }
 int getMinus(int a, int b) { return 0; }
 int getDivide(int a, int b) {
+    if (b == 0) {
+        throw std::runtime_error("Division is not allowed!");
+    }
+
     return a / b;
 }
 int getSumSum(int a, int b) { return 0; }
--- Sample-Test1/cal_test.cpp
+++ Sample-Test1/cal_test.cpp
@@ -4,3 +4,7 @@
 TEST(Calc, TestDivide) {
 	EXPECT_EQ(2, getDivide(2, 1));
 }
+
+TEST(Calc, TestDivide2) {
+	EXPECT_THROW(getDivide(10, 0), std::runtime_error);
+}
