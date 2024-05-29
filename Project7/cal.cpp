57edcd434311913c26efb005697f1f5950952747
kjw0384 <kjw0384@gmail.com>
2024-05-29 16:07:47 +0900

    누락 파일 upload

---
+++ Project7/cal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int getSum(int a, int b) { return 0; }
+int getGop(int a, int b) { return 0; }
+int getZegop(int a, int b) { return 0; }
+int getMinus(int a, int b) { return 0; }
+int getDivide(int a, int b) { return 0; }
+int getSumSum(int a, int b) { return 0; }
